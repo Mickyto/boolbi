9d2c26c678cddcecb5b36fdc05ec59afcd7ace35
Mickyto <egayigor@gmail.com>
2015-06-26 19:13:27 +0300

    Change bravito to bravito1

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 // New Code
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/bravito');
+var db = monk('localhost:27017/bravito1');
 
 
 var routes = require('./routes/index');
