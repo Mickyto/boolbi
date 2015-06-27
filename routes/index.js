9502a309201bb3fd1c09dccc459f5f80b09fb72d
Mickyto <egayigor@gmail.com>
2015-06-27 16:28:51 +0300

    Connection with DB

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,52 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET Hello World page. */
+/* GET New Ad page. */
 router.get('/newad', function(req, res) {
-  res.render('newad', { title: 'New Ad' })
+  res.render('newad', { title: 'New ad' })
+})
+
+/* GET Add ad page. */
+router.post('/addad', function(req, res) {
+  var db = req.db;
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+  var userTelephone = req.body.usertelephone;
+  var adTitle = req.body.adtitle;
+  var adDescription = req.body.addescription;
+  var adPrice = req.body.adprice;
+  var collection = db.get('adcollection');
+  collection.insert({
+    "Name" : userName,
+    "E-mail" : userEmail,
+    "Telephone" : userTelephone,
+    "Title" : adTitle,
+    "Description" : adDescription,
+    "Price" : "adPrice"
+      }, function (err, doc) {
+    if (err) {
+      res.send("There was a problem adding the information to the database.");
+     }
+    else {
+      res.redirect("ads");
+    }
+  });
 });
 
 
-router.get('/ad', function(req, res) {
-  res.render('ad', { title: 'Ad' })
+
+
+
+router.get('/ads', function(req, res) {
+  var db =req.db;
+  var collection = db.get('adcollection');
+  collection.find({},{},function(e,docs){
+      res.render('ads', {
+        "ads" : docs
+      })
+  })
 });
+
 
 
 module.exports = router;
