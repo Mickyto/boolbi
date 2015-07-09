af174cd2b5611c65d1474a4134138d1874a4a6d6
Игорь Эгай <egayigor@gmail.com>
2015-07-09 22:40:16 +0300

    bugs fix

--- routes/ads.js
+++ routes/ads.js
@@ -6,51 +6,48 @@ var fs = require('fs');
 var multipart = require('connect-multiparty');
 
 
+var multipartMiddleware = multipart();
+
 
 router.use(methodOverride(function(req, res){
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    var method = req.body._method
-    delete req.body._method
-    return method
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
   }
-}))
+}));
 
 
 /* GET New Ad page. */
 router.get('/newad', function(req, res) {
-  res.render('newad')
-})
+  res.render('newad');
+});
 
 
 /* GET Add ad page. */
-var multipartMiddleware = multipart();
 router.post('/addad', multipartMiddleware, function(req, res) {
 
-  var filePath = req.files.photo1.path;
-  var imageName = Math.random() + req.files.photo1.name;
-  var targetPath = './public/images/'+imageName;
-  fs.rename(filePath, targetPath, function(err) {
-      if (err) throw err;
-      fs.unlink(filePath, function() {
-          if (err) throw err;
-      });
-  });
+    if (req.files.photo1.name != '') {
+        var filePath = req.files.photo1.path;
+        var imageName = Math.random() + req.files.photo1.name;
+        var targetPath = './public/images/' + imageName;
+        fs.rename(filePath, targetPath, function (err) {
+            if (err) throw err;
+        });
+    }
 
-  var filePath2 = req.files.photo2.path;
-  var imageName2 = Math.random() + req.files.photo2.name;
-  var targetPath2 = './public/images/'+imageName2;
-  fs.rename(filePath2, targetPath2, function(err) {
-      if (err) throw err;
-      fs.unlink(filePath2, function() {
-          if (err) throw err;
-      });
-  });
-
-
+    if (req.files.photo2.name != '') {
+        var filePath2 = req.files.photo2.path;
+        var imageName2 = Math.random() + req.files.photo2.name;
+        var targetPath2 = './public/images/' + imageName2;
+        fs.rename(filePath2, targetPath2, function (err) {
+            if (err) throw err;
+        });
+    }
 
   var db = req.db;
-  var collection = db.get('adcollection');
-  collection.insert({
+  var colAds = db.get('adcollection');
+  colAds.insert({
     name : req.body.username,
     email : req.body.useremail,
     telephone : req.body.usertelephone,
@@ -71,9 +68,10 @@ router.post('/addad', multipartMiddleware, function(req, res) {
 
 
 router.get('/', function(req, res) {
+
   var db =req.db;
-  var collection = db.get('adcollection');
-  collection.find({},{},function(e, docs){
+  var colAds = db.get('adcollection');
+  colAds.find({},{},function(e, docs){
     res.render('ads', {
       'ads' : docs
     })
@@ -84,9 +82,10 @@ router.get('/', function(req, res) {
 
 // route middleware to validate :id
 router.param('id', function(req, res, next, id) {
+
   var db = req.db;
-  var collection = db.get('adcollection');
-  collection.findById(id, function (err, doc) {
+  var colAds = db.get('adcollection');
+  colAds.findById(id, function (err, doc) {
     if (err) {
       res.send(id + ' was not found');
     }
@@ -99,9 +98,10 @@ router.param('id', function(req, res, next, id) {
 
 
 router.get('/:id', function(req, res) {
+
   var db =req.db;
-  var collection = db.get('adcollection');
-  collection.findById(req.id, function (err, doc) {
+  var colAds = db.get('adcollection');
+  colAds.findById(req.id, function (err, doc) {
     if (err) {
       console.log('GET Error: There was a problem retrieving: ' + err);
     } else {
@@ -116,20 +116,19 @@ router.get('/:id', function(req, res) {
 
 //GET the individual ad by Mongo ID
 router.get('/:id/edit', function(req, res) {
+
   var db =req.db;
-  var collection = db.get('adcollection');
-  collection.findById(req.id, function (err, docs) {
+  var colAds = db.get('adcollection');
+  colAds.findById(req.id, function (err, docs) {
     if (err) {
       console.log('GET Error: There was a problem retrieving: ' + err);
     } else {
-          res.render('edit', {
-            'user' : docs
-          })
-        }
+      res.render('edit', {
+        'user' : docs
+      })
+    }
   })
 })
-
-
 
 
 //Update an ad by ID
@@ -162,8 +161,8 @@ router.post('/:id/adedit', multipartMiddleware, function(req, res) {
     }
 
     var db = req.db;
-    var collection = db.get('adcollection');
-    collection.findById(req.id, function (err, doc) {
+    var colAds = db.get('adcollection');
+    colAds.findById(req.id, function (err, doc) {
         var updateObject = {
             name: req.body.username,
             email: req.body.useremail,
@@ -182,7 +181,7 @@ router.post('/:id/adedit', multipartMiddleware, function(req, res) {
             var filePath2 = './public/images/' + doc.photo1;
             fs.unlinkSync(filePath2);
         }
-    collection.findAndModify({ _id : req.id}, {$set: updateObject})
+    colAds.findAndModify({ _id : req.id}, {$set: updateObject})
           .success(function () {
             res.redirect('/ads/' + req.id);
           })
@@ -190,12 +189,12 @@ router.post('/:id/adedit', multipartMiddleware, function(req, res) {
 });
 
 
-
 //DELETE a Ad by ID
 router.delete('/:id/edit', function (req, res){
+
   var db =req.db;
-  var collection = db.get('adcollection');
-  collection.findById(req.id, function (err, doc) {
+  var colAds = db.get('adcollection');
+  colAds.findById(req.id, function (err, doc) {
     if (err) {
       return err;
     } else {
@@ -203,7 +202,7 @@ router.delete('/:id/edit', function (req, res){
       var filePath2 = './public/images/' + doc.photo1;
       fs.unlinkSync(filePath1);
       fs.unlinkSync(filePath2);
-      collection.removeById(req.id,function (err, doc) {
+      colAds.removeById(req.id,function (err, doc) {
         if (err) {
           return err;
         } else {
