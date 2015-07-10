df254908b190b8bc48b30318a131a5aedcaa2acc
Игорь Эгай <egayigor@gmail.com>
2015-07-10 22:51:38 +0300

    merging callback of edit and added pages

--- routes/ads.js
+++ routes/ads.js
@@ -24,7 +24,7 @@ router.get('/newad', function(req, res) {
 });
 
 
-/* GET Add ad page. */
+/* GET Add ad page.
 router.post('/addad', multipartMiddleware, function(req, res) {
 
     if (req.files.photo1.name != '') {
@@ -65,6 +65,8 @@ router.post('/addad', multipartMiddleware, function(req, res) {
     }
   });
 });
+*/
+
 
 
 router.get('/', function(req, res) {
@@ -81,13 +83,13 @@ router.get('/', function(req, res) {
 
 
 // route middleware to validate :id
-router.param('id', function(req, res, next, id) {
+router.param('id', function (req, res, next, id) {
 
   var db = req.db;
   var colAds = db.get('adcollection');
-  colAds.findById(id, function (err, doc) {
-    if (err) {
-      res.send(id + ' was not found');
+  colAds.findById(id, function (err) {
+        if (err) {
+          res.send(id + ' was not found');
     }
     else {
       req.id = id;
@@ -105,7 +107,6 @@ router.get('/:id', function(req, res) {
     if (err) {
       console.log('GET Error: There was a problem retrieving: ' + err);
     } else {
-      console.log(doc);
           res.render('show', {
             'user': doc
           })
@@ -131,17 +132,19 @@ router.get('/:id/edit', function(req, res) {
 })
 
 
-//Update an ad by ID
-router.post('/:id/adedit', multipartMiddleware, function(req, res) {
+
+
+
+var adCallback = function(req, res) {
 
     if (req.files.photo1.name != '') {
 
-        var filePath = req.files.photo1.path;
+        var origPath = req.files.photo1.path;
         var imageName = Math.random() + req.files.photo1.name;
         var targetPath = './public/images/' + imageName;
-        fs.rename(filePath, targetPath, function (err) {
+        fs.rename(origPath, targetPath, function (err) {
             if (err) throw err;
-            fs.unlink(filePath, function () {
+            fs.unlink(origPath, function () {
                 if (err) throw err;
             });
         });
@@ -149,12 +152,12 @@ router.post('/:id/adedit', multipartMiddleware, function(req, res) {
 
     if (req.files.photo2.name != '') {
 
-        var filePath2 = req.files.photo2.path;
+        var origPath2 = req.files.photo2.path;
         var imageName2 = Math.random() + req.files.photo2.name;
         var targetPath2 = './public/images/' + imageName2;
-        fs.rename(filePath2, targetPath2, function (err) {
+        fs.rename(origPath2, targetPath2, function (err) {
             if (err) throw err;
-            fs.unlink(filePath2, function () {
+            fs.unlink(origPath2, function () {
                 if (err) throw err;
             })
         });
@@ -162,31 +165,46 @@ router.post('/:id/adedit', multipartMiddleware, function(req, res) {
 
     var db = req.db;
     var colAds = db.get('adcollection');
-    colAds.findById(req.id, function (err, doc) {
-        var updateObject = {
-            name: req.body.username,
-            email: req.body.useremail,
-            telephone: req.body.usertelephone,
-            title: req.body.adtitle,
-            description: req.body.addescription,
-            price: req.body.adprice
-        };
-        if (imageName != undefined) {
+    var updateObject = {
+        name: req.body.username,
+        email: req.body.useremail,
+        telephone: req.body.usertelephone,
+        title: req.body.adtitle,
+        description: req.body.addescription,
+        price: req.body.adprice
+    };
+        if (imageName !== undefined) {
             updateObject.mainphoto = imageName;
-            var filePath1 = './public/images/' + doc.mainphoto;
-            fs.unlinkSync(filePath1);
         }
-        if (imageName2 != undefined) {
+        if (imageName2 !== undefined) {
             updateObject.photo1 = imageName2;
-            var filePath2 = './public/images/' + doc.photo1;
-            fs.unlinkSync(filePath2);
         }
-    colAds.findAndModify({ _id : req.id}, {$set: updateObject})
-          .success(function () {
+    if (req.body.hiddenField !== undefined) {
+        colAds.findAndModify({_id: req.id}, {$set: updateObject})
+        .success(function () {
             res.redirect('/ads/' + req.id);
-          })
-    });
-});
+        })
+    } else {
+        colAds.insert(updateObject).success(function () {
+                res.redirect('/ads');
+        })
+    }
+};
+
+
+
+//if (hideField != undefined) {
+   // var filePath = './public/images/' + doc.mainphoto;
+  //  fs.unlinkSync(filePath);
+
+
+
+
+
+router.post('/addad', multipartMiddleware, adCallback);
+
+//Update an ad by ID
+router.post('/:id/adedit', multipartMiddleware, adCallback);
 
 
 //DELETE a Ad by ID
