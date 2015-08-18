43d363a0eb5b7b867915392390321e4dd84f9abc
Serkin Alexander <serkin.alexander@gmail.com>
2015-08-18 14:20:06 +0300

    Implement password hashing

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var passwordHash = require('password-hash');
+
 var router = express.Router();
 var nodemailer = require('nodemailer');
 var ObjectId = require('mongodb').ObjectId;
@@ -18,6 +20,7 @@ router.get('/', function(req, res) {
 
 
 router.get('/signup/', function(req, res) {
+
   res.render('user/login', {
     curPage: '/users/signup/',
     message : req.flash('info'),
@@ -32,48 +35,62 @@ router.post('/signup/', function(req, res) {
   var db = req.db;
   var userCol = db.get('users');
   var rand = Math.random();
-  var bodyEmail = req.body.useremail;
-  userCol.findOne({ email :  bodyEmail }, function(err, doc) {
+
+  var userEmail      = req.body.useremail ? req.body.useremail                        : null;
+  var userPassword   = req.body.password  ? passwordHash.generate(req.body.password)  : null;
+
+  userCol.findOne({ email :  userEmail }, function(err, doc) {
     if (doc) {
       req.flash('info', req.app.locals.i18n('exist'));
       res.redirect('/users/signup/');
-    } else {
-      userCol.insert({
-        email: bodyEmail,
-        password: req.body.password,
-        active : 'no',
-        secure_code : rand
-      }, function () {
-        var link ='http://' + req.get('host') + '/users/emailactivation?random=' + rand + '&email=' + bodyEmail;
-        var mailOptions = {
-          to: bodyEmail,
-          subject: "Please confirm your Email account",
-          html: '<head><style>a:hover{border: 2px solid white;}</style></head>' +
-          '<body style="background:#247BA7"><div align="center" style="height: 800px; padding: 50px">' +
-          '<div style="border: 2px solid green; border-radius: 20px;' +
-          'margin-top: 60px; margin-bottom: 60px; background-color: #60AB91;">' +
-          '<p align="center" style="font-family: Arial,Helvetica,sans-serif; font-size: 30px; color: gold;">' +
-          'Congratulations, you successfully created account on bravito.ru</p>' +
-          '<p align="center" style="font-family: Arial,Helvetica,sans-serif; font-size: 15px; color: white;">' +
-          'Bravito is a board of free ads where you can find necessary items<br> or services for few minutes<br>' +
-          'Now you can compose your own ads to sell items or to tell about your services</p><br>' +
-          '<p align="center" style="font-family: Arial,Helvetica,sans-serif; font-size: 20px; color: white;">' +
-          'Just one last action:</p><div align="center"><a href=' + link + ' style="font-size: 25px;' +
-          ' margin: 20px; background: goldenrod; border-radius: 10px; padding: 10px 40px 10px 40px;' +
-          'color: white; text-decoration: none">Click here to confirm your email</a></div>' +
-          '<p align="center", style="font-family: Arial,Helvetica,sans-serif;font-size: 30px; color: white;">' +
-          'Wish you successful sales</p></div></div></body>'
-        };
-        transporter.sendMail(mailOptions, function (err, res) {
-          if (err) {
-            res.render('default', { msg : 'Something was wrong' });
-          }
-        });
-        res.render('default', { msg : req.app.locals.i18n('check') });
 
-      });
     }
   });
+
+  if(userEmail === null || userPassword == null) {
+
+    req.flash('info', req.app.locals.i18n('wrong'));
+    res.redirect('/users/signup/');
+
+
+  } else {
+
+    userCol.insert({
+      email: userEmail,
+      password: userPassword,
+      active : 'no',
+      secure_code : rand
+    }, function () {
+      var link ='http://' + req.get('host') + '/users/emailactivation?random=' + rand + '&email=' + userEmail;
+      var mailOptions = {
+        to: userEmail,
+        subject: "Please confirm your Email account",
+        html: '<head><style>a:hover{border: 2px solid white;}</style></head>' +
+        '<body style="background:#247BA7"><div align="center" style="height: 800px; padding: 50px">' +
+        '<div style="border: 2px solid green; border-radius: 20px;' +
+        'margin-top: 60px; margin-bottom: 60px; background-color: #60AB91;">' +
+        '<p align="center" style="font-family: Arial,Helvetica,sans-serif; font-size: 30px; color: gold;">' +
+        'Congratulations, you successfully created account on bravito.ru</p>' +
+        '<p align="center" style="font-family: Arial,Helvetica,sans-serif; font-size: 15px; color: white;">' +
+        'Bravito is a board of free ads where you can find necessary items<br> or services for few minutes<br>' +
+        'Now you can compose your own ads to sell items or to tell about your services</p><br>' +
+        '<p align="center" style="font-family: Arial,Helvetica,sans-serif; font-size: 20px; color: white;">' +
+        'Just one last action:</p><div align="center"><a href=' + link + ' style="font-size: 25px;' +
+        ' margin: 20px; background: goldenrod; border-radius: 10px; padding: 10px 40px 10px 40px;' +
+        'color: white; text-decoration: none">Click here to confirm your email</a></div>' +
+        '<p align="center", style="font-family: Arial,Helvetica,sans-serif;font-size: 30px; color: white;">' +
+        'Wish you successful sales</p></div></div></body>'
+      };
+      transporter.sendMail(mailOptions, function (err, res) {
+        if (err) {
+          res.render('default', { msg : 'Something was wrong' });
+        }
+      });
+      res.render('default', { msg : req.app.locals.i18n('check') });
+
+    });
+  }
+
 });
 
 router.get('/emailactivation', function (req, res) {
@@ -106,23 +123,28 @@ router.get('/login', function(req, res) {
 router.post('/login', function (req, res) {
   var db = req.db;
   var userCol = db.get('users');
+
   userCol.findOne({email : req.body.useremail}, function (err, doc) {
 
-    if (doc && req.body.password === doc.password) {
-      if (doc.active == 'no') {
-        req.flash('info', 'Email wasn\'t activated');
-        res.redirect('/users/login');
-      } else {
-        req.session.user_id = doc._id;
-        req.session.email = doc.email;
+    if (doc && passwordHash.verify(req.body.password, doc.password)) {
 
-        res.redirect('/users/profile');
+      if (doc.active == 'no') {
+        req.flash('info', req.app.locals.i18n('inactiveUserAccount'));
+        res.redirect('/users/login');
+
+        return;
       }
-    } else {
-      req.flash('info', req.app.locals.i18n('wrong'));
-      res.redirect('/users/login');
+
+      req.session.user_id = doc._id;
+      req.session.email = doc.email;
+      res.redirect('/users/profile');
+
+      return;
+
     }
 
+      req.flash('info', req.app.locals.i18n('wrong'));
+      res.redirect('/users/login');
 
   });
 });
@@ -189,27 +211,29 @@ router.get('/edit', checkAuth, function (req, res) {
 
 router.post('/edit', checkAuth, function (req, res) {
 
-  if (req.body.newpass1 !== req.body.newpass2) {
-    req.flash('info', 'Passwords are not identical');
+  var userPassword = req.body.newpass2  ? passwordHash.generate(req.body.newpass2)  : null;
+
+  if (req.body.newpass1 != req.body.newpass2 || userPassword === null) {
+    req.flash('info', req.app.locals.i18n('userPasswordsNotIdentical'));
     res.redirect('/users/edit');
-  }
-  else {
-    var db = req.db;
-    var userCol = db.get('users');
-    var colObject = {
-      name: req.body.name,
-      telephone: req.body.telephone
-    };
 
-    if (req.body.newpass2 !== '') {
-      colObject.password = req.body.newpass2
-    }
-
-    userCol.findAndModify({_id: req.session.user_id}, {$set: colObject})
-        .success(function () {
-          res.redirect('/users/profile');
-        })
+    return;
   }
+
+  var db = req.db;
+  var userCol = db.get('users');
+  var colObject = {
+    name: req.body.name,
+    telephone: req.body.telephone,
+    password: userPassword
+  };
+
+
+  userCol.findAndModify({_id: req.session.user_id}, {$set: colObject})
+      .success(function () {
+        res.redirect('/users/profile');
+      })
+
 });
 
 
