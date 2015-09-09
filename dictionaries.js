b217136e4c6e8c0d0347f1dbabe4209774dd57f6
Игорь Эгай <egayigor@gmail.com>
2015-09-10 02:10:40 +0300

    some corrections

--- dictionaries.js
+++ dictionaries.js
@@ -64,7 +64,10 @@ module.exports = {
             alertDel: 'Вы уверены, что хотите удалить это объявление?',
             noPermission: 'У вас нет прав для этого действия',
             incorrectImage: 'Формат фотографии может быть только .jpeg или .png, а размер не более 2 Мб. Пожалуйса, проверьте свойства фотографии',
-            noAd: 'Не существует такого объявления'
+            noAd: 'Не существует такого объявления',
+            userInfo: 'Информация о продавце',
+            itemInfo: 'Информация о предложении',
+            editUser: 'Изменение профиля'
         };
         return russian[word];
     },
@@ -130,7 +133,10 @@ module.exports = {
             alertDel: 'Are you sure you want to delete this ad?',
             noPermission: 'You don\'t have permission for this action',
             incorrectImage: 'Format of image must be only .jpeg or .png amd size 2 Mb. Please, check parameters of image',
-            noAd: 'Such ad is not exist'
+            noAd: 'Such ad is not exist',
+            userInfo: 'Seller information',
+            itemInfo: 'Item information',
+            editUser: 'Edit profile'
         };
         return english[word];
     }
