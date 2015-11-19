9cd9016a426be58e59c153e97264041e224dcf05
Igor Egai <egayigor@gmail.com>
2015-11-19 14:43:23 +0500

    gitignore

--- shipitfile.js
+++ shipitfile.js
@@ -6,7 +6,7 @@ module.exports = function (shipit) {
             workspace: '/tmp/git-monitor',
             deployTo: '/boolbi',
             repositoryUrl: 'https://mickyto@bitbucket.org/mickyto/boolbi.git',
-            ignores: ['.git', '.gitignore', '.idea', '.vagrant', 'node_modules', 'nightwatch', 'npm-debug.log', 'tests', 'nightwatch.json', 'nodemon.json', 'readme.md', 'selenium-debug.log', 'Vagrantfile', 'node-bootstrap.sh', 'node_rc', 'reports', 'shipitfile.js', 'public/images/big/', 'public/images/small/'],
+            ignores: ['.git', '.gitignore', '.idea', '.vagrant', 'nightwatch', 'npm-debug.log', 'tests', 'nightwatch.json', 'nodemon.json', 'readme.md', 'selenium-debug.log', 'Vagrantfile', 'node-bootstrap.sh', 'node_rc', 'reports', 'shipitfile.js'],
             rsync: ['--del'],
             keepReleases: 2,
             key: '/home/vagrant/.ssh/id_rsa',
